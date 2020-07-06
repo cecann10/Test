cdeccb2eb074d1e38533995216dbc4e238090619
cecann10 <celina_plaza@yahoo.com>
2020-07-05 18:22:01 -0700

    moving around files for funzies

---
+++ test_master/test_directory/pyfile.py
@@ -0,0 +1,6 @@
+So many tests, 
+Not enough time
+
+Who has the time?
+We all have the time.
+how can I jump down a file?
---
+++ test_master/test_directory/pytile.py
@@ -0,0 +1,4 @@
+Are you my pytile?
+
+No, I am not the pytile you're
+looking for.
