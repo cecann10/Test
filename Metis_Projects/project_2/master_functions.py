eb9a5d65b46343def7fa05b23aee081e1c3bbf3c
cecann10 <celina_plaza@yahoo.com>
2020-07-08 22:10:18 -0700

    continued work on project 2 data web scraping

--- Metis_Projects/project_2/master_functions.py
+++ Metis_Projects/project_2/master_functions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[6]:
 
 
 import requests
@@ -11,17 +11,35 @@ import pandas as pd
 import numpy as np
 
 
-# In[19]:
+# In[48]:
 
 
-def make_model_df(url):
+def make_model_df(make, url):
     '''
-    make_car should be a string
+    Will return data for all cars listed on
+    any result page of using the 'browse by model' tool on 
+    https://www.fueleconomy.gov/feg/findacar.shtml.
+    Tool forces narrowed search by make and model,
+    so each result page scraped will be for a single
+    make and model.  
     
-    model_rank should be number in a string, 
-    unique for each model
+    Inputs:
+     - make = string of make of car's name
+     - url = url for scraping from results of from
+             https://www.fueleconomy.gov/feg/findacar.shtml
     
-    url should be a string
+    Output:
+    Dataframe for a single make & model of a car's:
+    - Year
+    - Model
+    - Engine Capacity
+    - Cylinders
+    - Transmissions
+    - Transmissions Speeds
+    - Fuel Types
+    - Greenhouse Gas Emissions
+    - Miles-per-gallon
+
     '''
     results = requests.get(url)
     page = results.text
@@ -29,76 +47,75 @@ def make_model_df(url):
     soup = BeautifulSoup(page, "html5lib")
 
     #initiate data storage
-    years = []
-    makes = []
-    models = []
-    vol_caps = []
-    cylinders = []
-    transmissions = []
-    powers = []
-    gges =[]
-    mpgs = []
+    years = []                   #year of car
+    models = []                  #model of car
+    engine_capacities = []       #engine capacity volume in liters
+    cylinders = []               #number of car's cylinders
+    transmissions = []           #car's transmission type
+    trans_speeds = []            #car's transmission speed
+    fuel_types = []              #fuel type the car takes
+    gg_emissions =[]             #greenhouse gas emissions from tailpipe of car in grams per mile
+    mpgs = []                    #miles-per-gallon of car
 
 
     for element in soup.find_all('td', colspan="8"):
 
         #year
-        for element_car in element.find_all('a'):
-            year = element_car.text.split()[0]
+        for car_info in element.find_all('a'):
+            year = car_info.text.split()[0]
             years.append(int(year))
-
-        #make
-        for element_car in element.find_all('a'):
-            make = element_car.text.split()[1]
-            makes.append(make)
         
-        #model NOTE THESE WILL NEED TO BE CLEANED IF MAKE HAD TWO WORDS
-        for element_car in element.find_all('a'):
-            model = ' '.join(element_car.text.split()[2:])
+        #model
+        for car_info in element.find_all('a'):
+            model = ' '.join(car_info.text.split()[2:])
             models.append(model)
 
-        # car combined volume capacity (vol_cap)
-        for element_description in element.find_all('span', class_="config"):
-            vol_cap = element_description.text.split(',')[0]
-            vol_caps.append(vol_cap)
+        
+        for car_details in element.find_all('span', class_="config"):
             
-        # cylinders
-        for element_description in element.find_all('span', class_="config"):
-            cylinder = element_description.text.split(',')[1]
+            # car combined volume capacity (vol_cap)
+            engine_capacity = float(car_details.text.split(',')[0].strip(" L"))
+            engine_capacities.append(engine_capacity)
+            
+            # cylinders
+            cylinder = int(car_details.text.split(',')[1].strip(' cyl'))
             cylinders.append(cylinder)
             
-        # transmission
-        for element_description in element.find_all('span', class_="config"):
-            transmission = (element_description.text.split(',')[2]).split(' ')[1]
+            # transmission
+            transmission = (car_details.text.split(',')[2]).split(' ')[1]
             transmissions.append(transmission)
             
-        # power/fuel source
-        for element_description in element.find_all('span', class_="config"):
-            power = element_description.text.split(',')[-1]
-            powers.append(power)
+            # transmission speed
+            trans_speed = ((car_details.text.split(',')[2]).split(' ')[2].strip(')(-spd'))
+            trans_speeds.append(trans_speed)
+
+            # fuel types (e.g. premium gas, electric, regular gas)
+            fuel_type = car_details.text.split(',')[-1]
+            fuel_types.append(fuel_type)
     
     
     #greenhouse gas emissions (tailpipe)
-    for element_description in soup.find_all('div', class_="ghg-score"):
-        gge = element_description.text
-        gges.append(gge)
+    for gge_element in soup.find_all('div', class_="ghg-score"):
+        gge = int(gge_element.text.strip(' grams/mile'))
+        gg_emissions.append(gge)
             
     # CAR MPG
-    for element_mpg in soup.find_all('td', class_="mpg-comb"):
-        mpg = element_mpg.text
-        mpgs.append(int(mpg))
+    for mpg_element in soup.find_all('td', class_="mpg-comb"):
+        mpg = int(mpg_element.text)
+        mpgs.append(mpg)
 
 
     #full dataframe        
     cars = pd.DataFrame({
     'year': years,
-    'make': makes,
+    'make': make,
     'model': models,
-    'vol_caps': vol_caps,
+    'capacity_liters': engine_capacities,
     'cylinders': cylinders,
     'transmission': transmissions,
-    'power': powers,
-    'gge': gges,
+    'trans_speed': trans_speeds,
+    'fuel_type': fuel_types,
+    'gg_emissions': gg_emissions,
     'mpg': mpgs,
     })
     
